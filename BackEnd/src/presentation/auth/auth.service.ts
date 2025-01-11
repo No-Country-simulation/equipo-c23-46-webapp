9d71dac4a1a17ba80868cbc69284c1633786e092
Dieguidev <diegogaraycullas@gmail.com>
2025-01-11 18:08:40 -0500

    feat: Añade funcionalidad en el servicio de registro de trabajadores con validaciones y manejo de errores

--- BackEnd/src/presentation/auth/auth.service.ts
+++ BackEnd/src/presentation/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Role } from '@prisma/client';
 import { BcryptAdapter } from '../../config';
 import { prisma } from '../../data/prisma/prisma-db';
 import { CustomError, RegisterStudentDto, StudentEntity } from '../../domain';
+import { RegisterWorkerDto } from '../../domain/dtos/auth/req-register-worker.dto';
 
 type HashFunction = (password: string) => string;
 type ConpareFunction = (password: string, hashed: string) => boolean;
@@ -63,6 +64,67 @@ export class AuthService {
       });
 
       return { student: StudentEntity.fromJson(user) };
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw CustomError.internalServer(`${error}`);
+    }
+  }
+
+  async registerWorker(registerWorkerDto: RegisterWorkerDto) {
+    const {
+      name,
+      lastName,
+      password,
+      email,
+      birthdate,
+      dni,
+      position,
+      salary,
+      start_date,
+      phone,
+    } = registerWorkerDto;
+
+    const [existEmail, exitsDni] = await Promise.all([
+      prisma.user.findUnique({ where: { email } }),
+      prisma.user.findUnique({ where: { dni } }),
+    ]);
+
+    if (existEmail) {
+      throw CustomError.badRequest('Email already exists');
+    }
+    if (exitsDni) {
+      throw CustomError.badRequest('Dni already exists');
+    }
+
+    const age = new Date().getFullYear() - new Date(birthdate).getFullYear();
+
+    try {
+      const hashPassword = this.hashPassword(password);
+      const user = await prisma.user.create({
+        data: {
+          role: Role.WORKER,
+          birthdate: new Date(birthdate).toISOString(),
+          dni,
+          age,
+          email,
+          lastName,
+          name,
+          password: hashPassword,
+          Worker: {
+            create: {
+              position,
+              salary,
+              start_date: new Date(start_date).toISOString(),
+              phone,
+            },
+          },
+        },
+        include: { Worker: true },
+      });
+
+      return { worker: user.Worker };
     } catch (error) {
       if (error instanceof CustomError) {
         throw error;
